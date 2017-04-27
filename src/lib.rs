23d4209241296a40f26f1a6e3bc9f83c664a54bb
Daniël Louwrink <daniel.louwrink@gmail.com>
2017-04-27 13:21:28 +0200

    prepare for usage as library

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ extern crate serde_json;
 
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 
 #[macro_use]
 extern crate components;
@@ -38,45 +37,10 @@ mod dragon;
 mod resultparser;
 mod grammarcompiler;
 
-use grammar::Grammar;
-use engine::*;
-use components::*;
-use std::fs::File;
-use std::io::Read;
 use errors::*;
-use futures::Stream;
 
-fn make_test_grammar() -> Grammar {
-    let mut file = File::open("test.json").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    serde_json::from_str(&contents).unwrap()
-}
-
-fn test() -> Result<()> {
-    env_logger::init().unwrap();
-
-    let _com = unsafe { com_initialize()? };
-
-    let engine = Engine::connect()?;
-
-    let registration = engine.register()?;
-
-    let test = registration.take(10).map(|event| {
-        println!("{:?}", event);
-        if let EngineEvent::Paused(cookie) = event {
-            engine.resume(cookie).unwrap();
-        }
-    });
-
-    for _ in test.wait() {
-    }
+pub fn initialize() -> Result<()> {
+    components::com_initialize()?;
 
     Ok(())
-}
-
-quick_main!(test);
-
-pub fn external_main() {
-    main();
 }
